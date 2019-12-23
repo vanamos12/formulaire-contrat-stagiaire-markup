db4bbf5c72b5fbcaf2ca31051fd2c9635eec7cfd
Poka Tchoneng Hermann <pokatchoneng@gmail.com>
2019-12-23 16:01:00 +0100

    sign.js doesn't work on my page

--- partiefinalestagiaire.php
+++ partiefinalestagiaire.php
@@ -27,55 +27,32 @@
             <h2>Partie G - Signatures</h2>
         </div>
         <div class="inscription-form">
-            <form action="" method="POST">
-                <h5>LE/LA Stagiaire</h5>
-                <canvas id="signaturestagiaire" width="250" height="200"></canvas>
-            </form>
+            <h5>LE/LA Stagiaire</h5>
+            <canvas id="signaturestagiaire" width="250" height="200"></canvas>
+            <button class ="btn btn-primary" id="resetSign">Annuler</button>
+        
         </div>
     </div>
 
-    <script type="text/javascript" src="js/jquery3.3.1.js">
+    <script type="text/javascript" 
+            src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+            crossorigin="anonymous">
     </script>
     <script type="text/javascript" src="js/app.js">
     </script>
+    <script type="text/javascript" src="js/sign.js">
+    </script>
     <script type="text/javascript">
         $(document).ready(function(){
-            var canvas = document.getElementById("signaturestagiaire")
-            var ctx = canvas.getContext("2d");
-            var mouseClickedIncanvas = false;
-            var mouseX = 0;
-            var mouseY = 0;
-            function draw(){
-                drawSignature();
-            }
-            function drawSignature(){
-                if (mouseClickedIncanvas){
-                    ctx.font = "20px Arial"
-                    ctx.fillStyle = "#000000"
-                    ctx.fillText(".", mouseX, mouseY)
-                }
-                requestAnimationFrame(draw)
-            }
-            function mouseDown(e){
-                mouseClickedIncanvas = true;
-                
-            }
-            function mouseUp(e){
-                if (mouseClickedIncanvas){
-                    mouseClickedIncanvas = false
-                }
-            }
-            function mouseMove(e){
-                mouseX = e.offsetX;
-                mouseY = e.offsetY;
-
-                console.log("mouseX", mouseX);
-                console.log("mouseY", mouseY);
-            }
-            canvas.addEventListener("mousedown", mouseDown, false)
-            canvas.addEventListener("mousemove", mouseMove, false)
-            document.addEventListener("mouseup", mouseUp, false)
-            //var interval = setInterval(draw, 1);
+            console.log("page fully loaded.");
+           $('#signaturestagiaire').sign({
+               resetButton: $('#resetSign'),
+               lineWidth: 20,
+               width:250,
+               height:200
+           })
+           console.dir({"hello": "Myhello"})
         })
     </script>
 
